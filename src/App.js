2cb39216b02f89e5acf615b0d00276f3a63d8db3
ADE <adejumoahmad4life@gmail.com>
2021-11-15 00:56:02 +0100

    imported files to app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 import "./App.css";
+import React from "react";
+// import {
+//   Footer,
+//   Blog,
+//   Possibility,
+//   Features,
+//   WhatGPT3,
+//   Header,
+// } from "./Containers";
+
+// import { Cta, Brand, Navbar } from "./Component";
+
+import Footer from "./Containers/footer/Footer";
+import Blog from "./Containers/blog/Blog";
+import Possibility from "./Containers/possibility/Possibility";
+import Features from "./Containers/features/Features";
+import WhatGPT3 from "./Containers/whatGPT3/WhatGPT3";
+import Header from "./Containers/header/Header";
 
 function App() {
   return (
     <div className="App">
-      <h1>GPT3</h1>
+      {/* <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <Cta />
+      <Blog />
+      <Footer /> */}
     </div>
   );
 }
